1108b626ee40078885cd7283acfac4adf8ac9d79
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-03 11:44:48 +0700

    menggunakan typescript di project next js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
-export default function Layout({ children }) {
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <head>
