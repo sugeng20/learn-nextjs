e4fcdb8be63d476314671c972060ba8b659885d1
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-03 11:26:28 +0700

    First commit project

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>Learn Next JS</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
