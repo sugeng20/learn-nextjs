6ac8bdd7bbbc66a59c1c165d67e1948054c9da75
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-04 21:36:18 +0700

    menggunakan static assets

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -3,6 +3,7 @@ export default function PostPage() {
   return (
     <>
       <Heading>Post Page</Heading>
+      <img src="/images/images-1.png" width={640} height={360} alt="" />
       <p>Hellow world, this is next.js app</p>
     </>
   );
--- app/blog/latihan-route-next/page.jsx
+++ app/blog/latihan-route-next/page.jsx
@@ -3,6 +3,7 @@ export default function LatihanRouteNextPage() {
   return (
     <>
       <Heading>Latihan Route Next Page</Heading>
+      <img src="/images/images-2.png" width={640} height={360} alt="" />
       <p>Halaman Latihan Route Next</p>
     </>
   );
