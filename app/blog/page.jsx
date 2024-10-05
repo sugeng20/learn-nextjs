b3c5ce1a74868778c0b9216210db4c43192e9410
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-05 23:36:15 +0700

    membuat fungsi untuk mendapatkan list content post

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,42 +1,30 @@
 import Heading from "@/components/Heading";
 import PostCard from "@/components/PostCard";
+import { getAllPosts } from "@/lib/post";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const posts = await getAllPosts();
+
   return (
     <>
       <Heading>Blog Page</Heading>
       <h2 className="text-2xl mb-3">List Of Posts</h2>
 
-      <PostCard
-        title="Belajar Next.js"
-        href="/blog/belajar-nextjs"
-        description="Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat
+      {posts.map((post, index) => {
+        return (
+          <PostCard
+            key={index}
+            title={post.title}
+            href="/blog/belajar-nextjs"
+            description="Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat
           vulputate. Ut vulputate est non quam dignissim elementum. Donec a
           ullamcorper diam."
-        date="13.01.2022"
-        author="Admin"
-        image="/images/image-1.png"
-      />
-      <PostCard
-        title="Belajar Next.js"
-        href="/blog/belajar-nextjs"
-        description="Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat
-          vulputate. Ut vulputate est non quam dignissim elementum. Donec a
-          ullamcorper diam."
-        date="13.01.2022"
-        author="Admin"
-        image="/images/image-2.png"
-      />
-      <PostCard
-        title="Belajar Next.js"
-        href="/blog/belajar-nextjs"
-        description="Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat
-          vulputate. Ut vulputate est non quam dignissim elementum. Donec a
-          ullamcorper diam."
-        date="13.01.2022"
-        author="Admin"
-        image="/images/image-1.png"
-      />
+            date={post.date}
+            author={post.author}
+            image={post.image}
+          />
+        );
+      })}
     </>
   );
 }
