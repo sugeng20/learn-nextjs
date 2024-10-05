5814b7f9d8e531b7a1e702137398a479539771f6
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-05 22:34:47 +0700

    cara terbaik menggunakan font di next.js

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,3 +1,9 @@
+import { roboto } from "@/app/fonts";
+
 export default function Heading({ children }) {
-  return <h1 className="text-2xl font-bold pb-3">{children}</h1>;
+  return (
+    <h1 className={`text-2xl font-bold pb-3 ${roboto.className}`}>
+      {children}
+    </h1>
+  );
 }
