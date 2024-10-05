7adcaed81966146c574d476c244a4144386f939f
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-05 23:20:48 +0700

    memisahkan layer data dengan layer ui

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -1,23 +1,19 @@
 import Heading from "@/components/Heading";
-import matter from "gray-matter";
-import { marked } from "marked";
-import { readFile } from "node:fs/promises";
-export default async function PostPage() {
-  const text = await readFile("./content/blog/belajar-nextjs.md", "utf-8");
-  const {
-    content,
-    data: { title, image, date, author },
-  } = matter(text);
-  const html = marked(content);
+import { getPost } from "@/lib/post";
 
+export default async function PostPage() {
+  const post = await getPost("belajar-nextjs");
   return (
     <>
-      <Heading>{title}</Heading>
+      <Heading>{post.title}</Heading>
       <p className="italic text-sm pb-2">
-        {date} by {author}
+        {post.date} by {post.author}
       </p>
-      <img src={image} width={640} height={360} alt="" className="mb-3" />
-      <article dangerouslySetInnerHTML={{ __html: html }} className="prose" />
+      <img src={post.image} width={640} height={360} alt="" className="mb-3" />
+      <article
+        dangerouslySetInnerHTML={{ __html: post.body }}
+        className="prose"
+      />
     </>
   );
 }
