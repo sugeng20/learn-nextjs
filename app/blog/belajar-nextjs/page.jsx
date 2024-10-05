f19314d7b5c6fe948edb2b36f551ac1681af4e00
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-05 22:55:08 +0700

    menampilkan style markdown yang sesuai dengan tailwindcss

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -1,12 +1,15 @@
 import Heading from "@/components/Heading";
+import { marked } from "marked";
 import { readFile } from "node:fs/promises";
 export default async function PostPage() {
   const text = await readFile("./content/blog/belajar-nextjs.md", "utf-8");
+  const html = marked(text);
+
   return (
     <>
       <Heading>Post Page</Heading>
       <img src="/images/images-1.png" width={640} height={360} alt="" />
-      <p>{text}</p>
+      <article dangerouslySetInnerHTML={{ __html: html }} />
     </>
   );
 }
