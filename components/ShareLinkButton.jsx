f3c7cf1d29e560572f2a478d5c7c0bf12af1babe
sugeng20 <sugeng.wanantara@gmail.com>
2024-10-06 23:11:59 +0700

    percantik halaman web dengan icon berbasis tailwindcss

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LinkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 export default function ShareLinkButton() {
@@ -14,8 +15,9 @@ export default function ShareLinkButton() {
   return (
     <button
       onClick={handleClick}
-      className="border px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700 mb-3"
+      className="border px-2 py-1 rounded flex items-center text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700 mb-3"
     >
+      <LinkIcon className="h-4 w-4 inline mr-1" />
       {copied ? "Link Copied" : "Share Link"}
     </button>
   );
